0c3cd19d65f03edff71950ce5f1b4b51da6ec573
Merty McGraw <mertymcgraw@gmail.com>
2017-06-15 16:06:46 -0700

    add methods to user model to calculate lifetime dive stats. Display livetime dive stats on user profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,10 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
   def show
+    @total_dives = current_user.dive_entries.length
+    @total_time_underwater = current_user.total_time_underwater
+    @max_depth = current_user.max_depth
+    @max_depth_location = current_user.max_depth_location
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def total_time_underwater
+    total_time = self.dive_entries.map do |entry|
+      entry.dive_time
+    end
+      total_time.reduce(:+)
+  end
+
+  def max_depth
+    max_depth = 0
+    self.dive_entries.each do |entry|
+      if entry.depth > max_depth
+        max_depth = entry.depth
+      end
+    end
+    max_depth
+  end
+
+  def max_depth_location
+    max_depth = 0
+    max_depth_dive_site= "N/A"
+    max_depth_location = "N/A"
+    self.dive_entries.each do |entry|
+      if entry.depth > max_depth
+        max_depth = entry.depth
+        max_depth_dive_site = entry.dive_site
+        max_depth_location = entry.location.location_name
+      end
+    end
+    "#{max_depth_dive_site}, #{max_depth_location}"
+  end
+
 end
